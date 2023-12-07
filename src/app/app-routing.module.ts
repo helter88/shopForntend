a1db1159d99572044175e97d306264ec57833a6a
artur-helta <artur.helta@onet.pl>
2023-12-07 16:59:55 +0100

    add layout for admin panel

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DefaultComponent } from './layouts/default/default.component';
-import { HomeComponent } from './modules/home/home.component';
-import { ProductComponent } from './modules/product/product.component';
 import { FullpageComponent } from './layouts/fullpage/fullpage.component';
+import { FullpageadminComponent } from './layouts/fullpageadmin/fullpageadmin.component';
+import { AdminComponent } from './modules/admin/admin.component';
+import { HomeComponent } from './modules/home/home.component';
 import { LoginComponent } from './modules/login/login.component';
+import { ProductComponent } from './modules/product/product.component';
 
 const routes: Routes = [{
   path: '', component: DefaultComponent, children: [{
@@ -12,10 +14,13 @@ const routes: Routes = [{
   }, {
     path: 'products', component: ProductComponent
   }]
-},
-{
+}, {
   path: '', component: FullpageComponent, children: [{
     path: 'login', component: LoginComponent
+  }]
+}, {
+  path: '', component: FullpageadminComponent, children: [{
+    path: 'admin', component: AdminComponent
   }]
 }];
 
---
+++ src/app/layouts/fullpageadmin/fullpageadmin.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FullpageadminComponent } from './fullpageadmin.component';
+import { AdminComponent } from 'src/app/modules/admin/admin.component';
+import { RouterModule } from '@angular/router';
+
+
+
+@NgModule({
+  declarations: [
+    FullpageadminComponent,
+    AdminComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ]
+})
+export class FullpageadminModule { }
